abd264ee3e378f8600dac28cc4f0f164c5e8cdf3
ricardosoaresds <rihcardosoares@gmail.com>
2022-06-29 20:15:07 -0300

    Finish all the requiriments and rule business of app

--- finapi-NODEJS/src/index.js
+++ finapi-NODEJS/src/index.js
@@ -29,7 +29,7 @@ function getBalance(statement) {
     if (operation.type === 'credit') {
       return acc + operation.amount
     } else {
-      return acc - operation.amout
+      return acc - operation.amount
     }
   }, 0)
 
@@ -105,6 +105,53 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation)
 
   return response.status(201).send()
+})
+
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const dateFormat = new Date(date + '00:00')
+
+  const statement = customer.statement.filter(
+    statement =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  )
+
+  return response.json(customer.statement)
+})
+
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  return response.json(customer)
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  // splice
+  customers.splice(customer, 1)
+
+  return response.status(200).json(customers)
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
 })
 
 app.listen(3333)
